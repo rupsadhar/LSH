a289de5183ffb2e2714dba50a021abc2d9b17330
Meganaa Reddy <50673735+Meganaa999@users.noreply.github.com>
2020-10-29 11:44:47 +0530

    Added comments

--- shingle.py
+++ shingle.py
@@ -3,12 +3,16 @@ import os
 import json
 import pandas as pd
 data_list=[]
-def parsing_data() :
-    data = pd.read_csv("./dna_data/human_data.txt", sep = "	")
 
+#parsing_data() returns the list of documents list from the dataset
+def parsing_data() :
+    #read data as .csv 
+    data = pd.read_csv("./dna_data/human_data.txt", sep = "	")
+    #each sequence stored as a list from the corpus data
     doc_list = data["sequence"].tolist()
     return doc_list
 
+#build_kmers returns the list of shingles of size 'ksize' from each document with id pasrsed to the parameter 'sequence'
 def build_kmers(sequence, ksize):
     kmers = []
     n_kmers = len(sequence) - ksize + 1
@@ -19,7 +23,10 @@ def build_kmers(sequence, ksize):
 
     return kmers
 
+#set of unique shingles obtained from each document in the corpus
 shingles={}
+
+# shingle returns the set of shingles stored and dumped in a json file 'shingle_list.json'
 def shingle():
     sequences = parsing_data()
     cnt=0
@@ -43,6 +50,6 @@ def shingle():
     with open("./shingle_list.json",'w') as ft:
         json.dump(list_of_unique_shingles, ft)
 
-
+#call the required functions
 parsing_data()
 shingle()
