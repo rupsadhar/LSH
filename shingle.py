5c6b49de56dd2b1c508da2bef6b4c02d8e071cff
Rupsa <rupsadhar22@gmail.com>
2020-10-29 10:20:31 +0530

    shingles

--- shingle.py
+++ shingle.py
@@ -13,12 +13,34 @@ def parsing_data() :
     # with open("doc_as_strings.json",'w') as f1:
     #     json.dump(result,f1)
     
-    data = pd.read_csv("./human_data.txt", sep = "	")
+    data = pd.read_csv("./dna_data/human_data.txt", sep = "	")
 
     doc_list = data["sequence"].tolist()
 # print(doc_list)
 
-    with open("./human_data.json",'w') as ft:
+    with open("./dna_data/human_data.json",'w') as ft:
         json.dump(doc_list, ft)
 
+def build_kmers(sequence, ksize):
+    kmers = []
+    n_kmers = len(sequence) - ksize + 1
+
+    for i in range(n_kmers):
+        kmer = sequence[i:i + ksize]
+        kmers.append(kmer)
+
+    return kmers
+
+shingles={}
+def shingle():
+    with open("./dna_data/human_data.json") as data:
+        sequences = json.load(data)
+    cnt=0
+    for i in sequences:
+        shingles[cnt]=build_kmers(i,10)
+        cnt+=1
+    with open("./shingles.json",'w') as ft:
+        json.dump(shingles, ft)
+
 parsing_data()
+shingle()
